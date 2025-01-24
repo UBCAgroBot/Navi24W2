69b5776f039d5197015282f4a506e432b5fbb799
Arman D <adrismir@gmail.com>
2025-01-23 21:24:30 -0800

    amazing progress bug tests dont pass >:(

--- arm/bug_algorithm.py
+++ arm/bug_algorithm.py
@@ -1,40 +1,54 @@
 import numpy as np
 from numpy.typing import NDArray
 
-class robot_pos:
+class Node:
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
 
-class goal_pos:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+def _can_move_up(maze: NDArray[np.int_], robot_pos: Node) -> bool:
+	if robot_pos.y - 1 < 0:
+		return False
 
-def make_bug_move(maze: NDArray[np.int_]) -> str:
-	"""
-	Given a 2d array of integers representing a 
-	maze where: 0 = empty space, 1 = obstacle, 
-	2 = robot, 3 = goal, returns either "UP",
-	"LEFT", "DOWN", "RIGHT" "STAY". The string signify 
-	which direction the algorithm wants to move
-	the robot.
-	"""
-	r_pos = None
-	for y in range (maze.shape[0]):
-		for x in range (maze.shape[1]):
-			if maze[y, x] == 2:
-				r_pos = robot_pos(x, y)
-	if not r_pos:
-		raise Exception("Cannot make_bug_move on board that does not have a robot")
+	return maze[robot_pos.y - 1, robot_pos.x] == 0
 
-	g_pos = None
-	for y in range(maze.shape[0]):
-		for x in range(maze.shape[1]):
-			if maze[y, x] == 3:
-				g_pos = goal_pos(x, y)
-	if not g_pos:
-		raise Exception("Cannot make_bug_move on board that does not have a goal square")
+def _can_move_left(maze: NDArray[np.int_], robot_pos: Node) -> bool:
+	if robot_pos.x - 1 < 0:
+		return False
+
+	return maze[robot_pos.y, robot_pos.x - 1] == 0
+
+def _can_move_down(maze: NDArray[np.int_], robot_pos: Node) -> bool:
+	if robot_pos.y + 1 >= maze.shape[0]:
+		return False
+
+	return maze[robot_pos.y + 1, robot_pos.x] == 0
+
+def _can_move_right(maze: NDArray[np.int_], robot_pos: Node) -> bool:
+	if robot_pos.x + 1 >= maze.shape[1]:
+		return False
+
+	return maze[robot_pos.y, robot_pos.x + 1] == 0
+
+def get_bug_move(maze: NDArray[np.int_], robot_pos: tuple[int, int], goal_pos: tuple[int, int]) -> str:
+	"""
+	Parameters:
+		maze: A 2d array of integers representing the obstacles in the maze.
+		      0 represents empty space and 1 represents an obstacle
+		robot_pos: A tuple represeting the (x, y) coordinates of the robot.
+		           The coordinates are zero indexed
+		goal_pos: A tuple representing the (x, y) coordinates of the goal.
+		          The coordinates are zero indexed
+
+	This function will decide which direction the bug should move to get 
+	to the goal node.
+
+	Returns:
+		One of: "UP", "LEFT", "DOWN", "RIGHT", "STAY"
+	"""
+
+	r_pos = Node(robot_pos[0], robot_pos[1])
+	g_pos = Node(goal_pos[0], goal_pos[1])
 
 	desired_direction = None
 	x_dist = g_pos.x - r_pos.x
@@ -42,6 +56,7 @@ def make_bug_move(maze: NDArray[np.int_]) -> str:
 
 	if x_dist == 0 and y_dist == 0:
 		desired_direction = "STAY"
+
 	elif abs(x_dist) > abs(y_dist):
 		# We are further away horizontally
 		if x_dist > 0:
@@ -55,47 +70,99 @@ def make_bug_move(maze: NDArray[np.int_]) -> str:
 		else:
 			desired_direction = "UP"
 
+	if desired_direction == "UP" and not _can_move_up(maze, r_pos):
+		if _can_move_left(maze, r_pos):
+			desired_direction = "LEFT"
+		if _can_move_right(maze, r_pos):
+			desired_direction = "RIGHT"
+		if _can_move_down(maze, r_pos):
+			desired_direction = "DOWN"
+
+	if desired_direction == "LEFT" and not _can_move_left(maze, r_pos):
+		if _can_move_up(maze, r_pos):
+			desired_direction = "UP"
+		if _can_move_down(maze, r_pos):
+			desired_direction = "DOWN"
+		if _can_move_right(maze, r_pos):
+			desired_direction = "RIGHT"
+
+	if desired_direction == "DOWN" and not _can_move_down(maze, r_pos):
+		if _can_move_left(maze, r_pos):
+			desired_direction = "LEFT"
+		if _can_move_right(maze,r_pos):
+			desired_direction = "RIGHT"
+		if _can_move_up(maze,r_pos):
+			desired_direction = "UP"
+
+	if desired_direction == "RIGHT" and not _can_move_right(maze, r_pos):
+		if _can_move_up(maze, r_pos):
+			desired_direction = "UP"
+		if _can_move_down(maze, r_pos):
+			desired_direction = "DOWN"
+		if _can_move_left(maze, r_pos):
+			desired_direction = "LEFT"
+
 	return desired_direction
 
-def test_make_bug_move():
-	maze1 = np.array([
+def test_get_bug_move():
+	empty_3x3_maze = np.array([
 		[0, 0, 0],
-		[2, 0, 3],
+		[0, 0, 0],
 		[0, 0, 0],
 	])
-	m_maze1 = make_bug_move(maze1)
+	m_maze1 = get_bug_move(empty_3x3_maze, (0, 1), (2, 1))
 	if m_maze1 != "RIGHT":
 		print(m_maze1)
-		raise Exception("make_bug_move should have returned RIGHT")
+		raise Exception("get_bug_move should have returned RIGHT")
 
-
-	maze2 = np.array([
-		[0, 0, 0],
-		[3, 0, 2],
-		[0, 0, 0],
-	])
-	m_maze2 = make_bug_move(maze2)
+	m_maze2 = get_bug_move(empty_3x3_maze, (2, 1), (0, 1))
 	if m_maze2 != "LEFT":
-		raise Exception("make_bug_move should have returned LEFT")
-	
-	maze3 = np.array([
-		[0, 3, 0],
-		[0, 0, 0],
-		[0, 2, 0],
-	])
-	m_maze3 = make_bug_move(maze3)
-	if m_maze3 != "UP":
-		raise Exception("make_bug_move should have returned UP")
+		raise Exception("get_bug_move should have returned LEFT")
 
-	maze4 = np.array([
-		[0, 2, 0],
+	m_maze3 = get_bug_move(empty_3x3_maze, (1, 2), (1, 0))
+	if m_maze3 != "UP":
+		print(m_maze3)
+		raise Exception("get_bug_move should have returned UP")
+
+	m_maze4 = get_bug_move(empty_3x3_maze, (1, 0), (1, 2))
+	if m_maze4 != "DOWN":
+		raise Exception("get_bug_move should have returned DOWN")
+
+	obstacle_maze = np.array([
 		[0, 0, 0],
-		[0, 3, 0],
+		[1, 1, 0],
+		[0, 0, 0],
 	])
-	m_maze4 = make_bug_move(maze4)
-	if m_maze4!= "DOWN":
-		raise Exception("make_bug_move should have returned DOWN")
+	m_maze5 = get_bug_move(obstacle_maze, (0, 0), (0, 2))
+	if m_maze5 != "RIGHT":
+		print(m_maze5)
+		raise Exception("get_bug_move should have returned RIGHT")
+
+	m_maze6 = get_bug_move(obstacle_maze, (2, 1), (0, 0))
+	if m_maze6 != "UP":
+		print(m_maze6)
+		raise Exception("get_bug_move should have returnd UP")
+
+	circle_maze = np.array([
+	    [0, 0, 0],
+	    [0, 1, 0],
+	    [0, 0, 0],
+	])
+	m_maze7 = get_bug_move(circle_maze, (2, 2), (0, 1))
+	if m_maze7 != "LEFT":
+		raise Exception("get_bug_move should have returned LEFT")
+
+	alcove = np.array([
+	    [0, 0, 0, 0],
+	    [1, 1, 1, 0],
+	    [0, 0, 1, 0],
+	    [0, 1, 1, 0],
+	    [0, 0, 0, 0],
+	])
+	m_maze8 = get_bug_move(alcove, (1, 2), (3, 0))
+	if m_maze8 != "LEFT":
+		raise Exception("get_bug_move should have returned LEFT")
 
 if __name__ == "__main__":
-	test_make_bug_move()
-	print("Add make_bug_move tests passed")
+	test_get_bug_move()
+	print("Add get_bug_move tests passed")
