c14f5577a4b8165458807e8b72d7dad06a5d45f9
sayyamsingla <singlasayyam@gmail.com>
2025-03-29 13:46:13 -0700

    log formatting changes

--- arm/continuous_env/continuous_env/log.py
+++ arm/continuous_env/continuous_env/log.py
@@ -8,9 +8,9 @@ class sars(BaseModel):
 	r: Any
 	s_prime: Any
 
-entries:list[sars]=  []
+entries:list[tuple[int,int]]=  []
 
-def save_sars(s, a, r, s_prime, log_file_name, batch_size = 10000):
+def save_sars(s, a, r, s_prime, log_file_name, batch_size = 10):
 	"""
 	Saves the s, a, r, s_prime value to a file in the log
 	folder under the same log_file_name
@@ -18,10 +18,48 @@ def save_sars(s, a, r, s_prime, log_file_name, batch_size = 10000):
 	ie. Commas seperate the values and a semicolon seperates
 	entries
 	"""
+	# global entries
+	# for nested_arr in s:
+	# 	for val in nested_arr:
+	# 		print(val, end = "")
+	# 		if val 
+
 	global entries
-	entries.append(sars(s=s, a=a, r=r, s_prime=s_prime))
+	for i, nested_arr in enumerate(s):
+		for j, val in enumerate(nested_arr):
+			print(val, end = "")
+		print()
+
+	global entries
+	x = None
+	y = None
+
+	for i, nested_arr in enumerate(s):
+		for j, val in enumerate(nested_arr):
+			print(val, end = "")
+			if(val == 1):
+				x = i
+				y = j
+		print(f" (width of {len(nested_arr)})")	
+
+
+	print(f"(length of s {len(s)})")
+
+	if x == None or y == None:
+		print("Could not find robot position")
+	else:
+		print("robot position (x,y): ", x, y)
+		
+	# entries.append(sars(s=s, a=a, r=r, s_prime=s_prime))
+
+	entries.append((x,y))
+	# print(len(entries))
 	if len(entries) > batch_size:
 		log_file_path = os.path.join("logs", log_file_name)
 		with open(log_file_path, "a") as f:
-			f.write(";".join(f"{e.s},{e.a},{e.r},{e.s_prime}" for e in entries) + ";")
+			# f.write(x , y)
+			for entry in entries:
+				f.write(f"({entry[0]}, {entry[1]}) ")
+			
+			# f.write(";".join(f"{e.s},{e.a},{e.r},{e.s_prime}" for e in entries) + ";")
 		entries.clear()
